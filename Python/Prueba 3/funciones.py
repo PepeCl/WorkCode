5a0fe9765fe1e524c5ce0f991906a811dd67c760
PepeCl <giu.saavedra@duocuc.cl>
2024-07-02 14:16:10 -0400

    gola

---
+++ Python/Prueba 3/funciones.py
@@ -0,0 +1,47 @@
+"""
+Puede escribir aqui las funciones del codigo
+Se importaran de forma automatica al 'main.py'
+"""
+# ------ NO BORRAR -----
+def test () -> None:
+    """
+        Funcion para probar el archivo
+    """
+    import herramientas
+    menu = herramientas.load_items('menu.csv')
+    print(menu)
+
+# esto es para que test solo corra si es ejecutado directamente
+if __name__ == '__main__':
+    test()
+# ------ NO BORRAR -----
+
+#Escribir Funciones desde aqui hacia abajo ------------
+carrito_user = []
+#ejemplo de funcion
+def revisar_menu(lista_menu):
+    for comidas in lista_menu:
+        print(f"{comidas["nombre"]}---> Precio = {comidas["precio"]}")
+    while True:
+        opcion = input("Desea agregar alguna comida a su carrito de compras? (s/n): ")
+        if opcion == "s":
+            item = input("Seleccione alguna comida que desee agregar al carrito: ")
+            for elemento in lista_menu:
+                if item in elemento.values():
+                    carrito_user.append(elemento)
+        if opcion == "n":
+            break
+
+def buscar_ingrediente(lista_carrito,ingrediente):
+    for comida in lista_carrito:
+        ing = comida["ingredientes"]
+        if ingrediente in ing:
+            print(comida)
+
+
+
+
+
+
+
+
---
+++ Python/Prueba 3/main.py
@@ -0,0 +1,54 @@
+# ------ NO BORRAR -----
+from funciones import *
+from herramientas import *
+# ------ NO BORRAR -----
+
+user = {}
+menu = ["Revisar menú",
+        "Revisar carrito",
+        "Pagar",
+        "Quitar item de carrito",
+        "Buscar item en carrito por nombre",
+        "Buscar item en carrito por ingredientes",
+        "Filtrar items que den alergia",
+        "Burcar ítem",
+        "Modificar ítem del menú",
+        "Agregar ítem al menú",
+        "Guardar carrito",
+        "Cargar carrito",
+        "Calcular calorías",
+        "Salir"]
+
+#Acciones iniciales
+
+
+#menu principal
+while (True):
+    for i, opcion in enumerate(menu):
+        print(f"{i+1}:{opcion}")
+    
+    while True:
+        accion = int(input("Ingrese la acción a realizar: "))
+        seleccion = menu[accion-1]
+        break
+
+    print(seleccion)
+
+    if accion == 1:
+        revisar_menu(var)
+    if accion == 2:
+        if carrito_user:
+            print(carrito_user)
+        else:
+            print("El usuario no ha agregado comidas para comprar")
+
+    if accion == 3:
+        while True:
+            ingrediente = input("Ingrese el ingrediente a buscar: ")
+            buscar_ingrediente(var,ingrediente)
+            opcion = input("Desea buscar otro ingrediente? (s/n): ")
+            if opcion == "s":
+                continue
+            if opcion == "n":
+                break
+            
